b2d4c3c259401d2f72a71193575007ee6119230a
wjt17600279551 <m17600279551@163.com>
2018-11-22 10:25:06 +0800

    react-00

---
+++ src/comtainers/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import RouterView from "router";
+
+import 'common/css/bootstrap.min.css'
+import 'common/css/common.css'
+import 'common/css/reset.css'
+import 'common/css/style.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="wraper">
+      
+      </div>
+    );
+  }
+}
+
+export default App;
